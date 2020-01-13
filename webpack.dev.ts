c629f004cc3553fbc786f670c3a0cfb39c6ced0d
André Soares <codigoaberto.info@gmail.com>
2020-01-13 14:09:51 -0300

    chore: add full dependencies

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -2,21 +2,25 @@ import * as webpack from "webpack";
 import * as HtmlWebPackPlugin from "html-webpack-plugin";
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./public/index.html"
+  template: "./public/index.html"
 });
 
 const config: webpack.Configuration = {
-    mode: "development",
-    entry: "./src/index.tsx",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ]
-    },
-    plugins: [htmlPlugin]
-}
+  mode: "development",
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
 
 export default config;
--- webpack.prod .ts
+++ webpack.prod .ts
@@ -3,26 +3,30 @@ import * as webpack from "webpack";
 import * as HtmlWebPackPlugin from "html-webpack-plugin";
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./public/index.html",
-    filename: "./index.html"
+  template: "./public/index.html",
+  filename: "./index.html"
 });
 
 const config: webpack.Configuration = {
-    mode: "production",
-    entry: "./src/index.tsx",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ]
-    },
-    plugins: [htmlPlugin]
-}
+  mode: "production",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
 
 export default config;
